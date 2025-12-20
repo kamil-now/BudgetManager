68edcf0ca8a8c5864076059da9abfb7a8cc2acb4
Kamil Nowak <kamil.nowak@mail.com>
2025-12-20 09:29:29 +0100

    update packages

--- src/BudgetManager.Api/Program.cs
+++ src/BudgetManager.Api/Program.cs
@@ -6,7 +6,7 @@ using BudgetManager.Infrastructure.Configuration;
 using BudgetManager.Infrastructure.Persistence;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.FileProviders;
-using Microsoft.OpenApi.Models;
+using Microsoft.OpenApi;
 
 var builder = WebApplication.CreateBuilder(args);
 
